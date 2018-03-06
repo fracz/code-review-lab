58999859c156e7c9f264c5df03357f06f6da452f
Wojciech Frącz <fraczwojciech@gmail.com>
2018-03-06 22:45:31 +0100

    Almost working

--- README.md
+++ README.md
@@ -7,4 +7,21 @@ docker-compose up gerrit
 # Ctrl+C when initialized
 # commend back the ENTRYPOINT line
 docker-compose up --build -d gerrit
+# wait until app starts
+docker logs labcr-gerrit
+docker-compose up --build -d
 ```
+
+1. Create new `labcr` project with initial empty commit.
+1. Go to `All-Projects` Access tab and remove `Read` persmission for
+   Anonymous users. Add `Read` permission for Registered users. 
+1. Create an account for students:
+    ```
+    username: student
+    email: student@student.agh.edu.pl
+    ```
+1. Go to Preferences / HTTP password and generate one that does not contain
+`/` or `+` characters.
+1. Put it in `get-change.php` constant.
+1. Adjust the rest of constants in this script.
+1. Run `php get-change.php` once to see if it works.
--- get-change.php
+++ get-change.php
@@ -1,20 +1,25 @@
 <?php
 define('REPO', __DIR__ . '/labcr');
 define('URL', 'http://apps.iisg.agh.edu.pl:12345');
+define('STUDENT_PASSWORD', 'cOrh8T2mOLHtlXpJhxfxtwNv83wsSJHTeoZOzMkFZw');
 
 require 'vendor/autoload.php';
 
 $lock = new \TH\Lock\FileLock('lock.lock');
 
-$acquired = false;
 
-$task = 1;//intval($_GET['task']) - 1;
-in_array($task, [0, 1]) or die();
+$task = intval($_GET['task']) - 1;
+$author = str_replace('"', '', $_GET['author']);
+if (php_sapi_name() == 'cli') {
+    $task = 0;
+    $author = 'Wojciech Frącz';
+}
+in_array($task, [0, 1]) or die("Invalid task");
 $task += 1;
-$author = 'AA';//str_replace('"', '', $_GET['author']);
 // uncomment to have diacritic characters work on windows
 // $author = iconv('UTF-8', 'windows-1250', $author);
 
+$acquired = false;
 while (!$acquired) {
     try {
         $lock->acquire();
@@ -24,13 +29,26 @@ while (!$acquired) {
     }
 }
 
-$commands = [
+$commands = [];
+
+if (!file_exists(REPO)) {
+    exec('mkdir labcr');
+    $commands = [
+        'cp -R ../labcr-template .git',
+        'git init',
+        'git remote add origin http://student:' . STUDENT_PASSWORD . '@gerrit:8080/labcr',
+        'git fetch',
+        'git reset --hard origin/master'
+    ];
+}
+
+$commands = array_merge($commands, [
     'git reset --hard origin/master',
     'cp -R ' . __DIR__ . '/tasks/task' . $task . '/* .',
     'git add -A',
     'git commit -m "Task ' . $task . ' - ' . $author . '"',
     'git push origin HEAD:refs/for/master',
-];
+]);
 
 $changeIdRegex = '#remote:\s*http://.+/(\d+)#';
 
